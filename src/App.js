9391c6f4f52ab39d5f51c372da60faa7376567c0
Kiranabrar <kiran.cs577@gmail.com>
2023-08-25 14:37:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
        <Routes>
        
         <Route exact path="/" element={<Create/>}></Route>
-        <Route exact path="/read" element={<Read/>}></Route>
-        <Route exact path="/update" element={<Update/>}></Route>
+        <Route  path="/read" element={<Read/>}></Route>
+        <Route  path="/update" element={<Update/>}></Route>
         
        </Routes>
       </BrowserRouter>
